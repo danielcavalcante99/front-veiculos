c598ed8d27c7903dc9ca5ea4a1067cb55680b304
danielcavalcante99 <daniel16henrrique@gmail.com>
2024-09-26 16:27:10 -0300

    Index

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import HomePage from "./pages/home/HomePage";
+import NavBar from "./components/NavBar/NavBar";
+import 'react-toastify/dist/ReactToastify.min.css';
+import {ToastContainer} from "react-toastify";
 
 function App() {
   return (
     <div className="App">
-
+        <NavBar/>
+        <HomePage/>
+        <ToastContainer />
     </div>
   );
 }
